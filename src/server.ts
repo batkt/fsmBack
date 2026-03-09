2af5dee1176af19e68e4db28cb02c488ea70e12d
buynaaaa24 <abuynaa1211@gmail.com>
2026-03-09 10:55:10 +0800

    a

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
