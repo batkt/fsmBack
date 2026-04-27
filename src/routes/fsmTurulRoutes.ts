7aa295e227fc377aa8409395e20178f62d0e5897
buynaaaa24 <abuynaa1211@gmail.com>
2026-04-27 08:58:17 +0800

    t

--- src/routes/fsmTurulRoutes.ts
+++ src/routes/fsmTurulRoutes.ts
@@ -7,11 +7,13 @@ import {
 } from "../controllers/fsmTurulController";
 import { validateFSMAccess } from "../middlewares/fsmAccess";
 
+import { authMiddleware } from "../middlewares/auth";
+
 const router = Router();
 
-router.get("/fsm-turuls", validateFSMAccess, getTuruls);
-router.post("/fsm-turuls", validateFSMAccess, createTurul);
-router.put("/fsm-turuls/:id", validateFSMAccess, updateTurul);
-router.delete("/fsm-turuls/:id", validateFSMAccess, deleteTurul);
+router.get("/fsm-turuls", authMiddleware, validateFSMAccess, getTuruls);
+router.post("/fsm-turuls", authMiddleware, validateFSMAccess, createTurul);
+router.put("/fsm-turuls/:id", authMiddleware, validateFSMAccess, updateTurul);
+router.delete("/fsm-turuls/:id", authMiddleware, validateFSMAccess, deleteTurul);
 
 export default router;
